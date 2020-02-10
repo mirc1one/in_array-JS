41dceeaa9109e4630fbbb51ff67d1953eb0bd49f
Mircea Bratu <44461660+mirc1one@users.noreply.github.com>
2020-02-10 11:19:27 +0200

    Update in_array.js

--- in_array.js
+++ in_array.js
@@ -1,5 +1,33 @@
+// in_array that performs just like in PHP
+// Functional
 function in_array(needle, haystack) {
-    if (typeof needle === 'string') {
+    if (typeof needle === 'string' || typeof needle === 'number') {
+        for (let i in haystack) {
+            if (haystack[i] === needle) {
+                return true;
+            }
+        }
+    } else if (typeof needle === 'object') {
+        for (let i in needle) {
+            for (let j in haystack) {
+                if (needle[i] === haystack[j]) {
+                    return true;
+                }
+            }
+        }
+    } else {
+        return null;
+    }
+    
+    return false;
+}
+
+// Or use it as prototype in JavaScript
+// Prototype
+Array.prototype.in_array = String.prototype.in_array = function(needle) {
+    const haystack = this;
+
+    if (typeof needle === 'string' || typeof needle === 'number') {
         for (let i in haystack) {
             if (haystack[i] === needle) {
                 return true;
